19f4603bf66ef569fc1a86503a0c8f3871318e94
iodein <ken.record1210@gmail.com>
2023-12-29 16:39:02 +0900

    アクセス<style調整>

--- src/views/AccessView.vue
+++ src/views/AccessView.vue
@@ -2,7 +2,7 @@
   <div class="access">
     <h1>テーラードイ：アクセス（地図）案内</h1>
     <div class="map">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1590.9219162121105!2d135.50989064312776!3d34.63815766028666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000dde97ee13501%3A0x471aea7336885a99!2z44OG44O844Op44O844OJ44Kk!5e0!3m2!1sja!2sjp!4v1703834412914!5m2!1sja!2sjp" width="600" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1590.9219162121105!2d135.50989064312776!3d34.63815766028666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000dde97ee13501%3A0x471aea7336885a99!2z44OG44O844Op44O844OJ44Kk!5e0!3m2!1sja!2sjp!4v1703834412914!5m2!1sja!2sjp" width="600" height="350" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
     <h2>お洋服ねっと　お仕立/お直し専門店</h2>
     <p>店主:土井 義弘
